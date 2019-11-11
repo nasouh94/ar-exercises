18aafdc02c8e6f1cb4a813dc7e101426c18be0d0
nasouh94 <nasouh@live.com>
2019-11-11 22:10:50 +0000

    exercise 5 solved

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+
+average_revenue = Store.average(:annual_revenue)
+
+puts "this is the average revenue for all stores #{average_revenue}"
+
+million_revenue = Store.where("annual_revenue < ?",1000000).count
+
+puts "number of stores with over a million $ in revnue #{million_revenue}"
